e523f9449656f0372e6a38e5e5949cd8447503ee
Ben Smith <ben.bradley.smith@gmail.com>
2013-07-30 20:37:45 +0100

    Added sudo to composer install command

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+sudo apt-get install -y git-core
+sudo git clone https://github.com/BenBradley/ec2-nginx-setup
+
+sudo mv ec2-nginx-setup/ec2/* .
+sudo rm -rf ec2-nginx-setup/
+
+sed -ie s/sitename/QuoteGoat/g setup.sh
+sed -ie s/sitename/QuoteGoat/g laravel.conf
+sed -ie s/hostnameholder/Hydrogen/g setup.sh
+sed -ie s/ipaddress/54.229.121.178/g setup.sh
+
+sudo rm setup.she
+sudo rm laravel.confe
+
+sudo chmod 777 setup.sh
+./setup.sh
---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Change the host name, replace ip and host name with correct information
+sudo chmod 777 /etc/hosts
+sudo echo "ipaddress hostnameholder" >> /etc/hosts
+sudo chmod 644 /etc/hosts
+
+sudo chmod 777 /etc/hostname
+sudo echo "hostnameholder" > /etc/hostname
+sudo chmod 644 /etc/hostname
+sudo hostname -F /etc/hostname
+
+# Update and upgrade packages using apt-get
+sudo apt-get update -y
+sudo apt-get upgrade -y --show-upgraded
+
+# Install git and curl 
+sudo apt-get install -y git-core
+sudo apt-get install -y git
+sudo apt-get install -y curl
+
+# Install nginx and php
+sudo apt-get install -y nginx php5-fpm php5-cli php5-mcrypt
+
+# Move the laravel.conf file to the nginx sites-enabled directory 
+sudo mv ~/laravel.conf /etc/nginx/sites-enabled/sitename.com
+
+# Install mysql
+sudo apt-get install -y mysql-server php5-mysql
+sudo mysql_secure_installation
+
+# Make the main site directory and logs subdirectory
+sudo git clone git://github.com/BenBradley/Laravel-4-Bootstrap.git sitename
+sudo mkdir sitename/logs
+
+# Symbollically link the site directory to enable the site and then restart the server
+sudo ln -s /etc/nginx/sites-available/example.com /etc/nginx/sites-enabled
+sudo /etc/init.d/nginx restart
+
+# Install emacs
+sudo add-apt-repository -y ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs-snapshot
+
+# Install composer and then use composer to install all dependencies
+sudo curl -s http://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+cd sitename
+sudo composer install --dev
+
+# Set up database
+sudo mysqladmin -u root -p create sitename
+
+# Change permissions on the app/storage directory
+sudo chmod -R 777 app/storage
+
+# Set encryption key
+sudo php artisan key:generate
+
+# Build assets using bassett
+sudo php artisan basset:build
+
+cd ~
