948606c7353c91b3b3e1503cdad35c9ca886145b
Simon Maxwell-Stewart <smaxwellstewart@gmail.com>
2014-02-10 09:34:08 -0500

    Made final alteration before testing

--- boom.sh
+++ boom.sh
@@ -7,23 +7,13 @@ echo "Starter Repo:"
 read repo
 echo ""
 
-
-# Make some swap
-sudo dd if=/dev/zero of=/var/swapfile bs=1024 count=1024k
-sudo mkswap /var/swapfile
-sudo swapon /var/swapfile
-
-# Install git using apt-get
-sudo apt-get install -y git-core
-
 # Clone this directory onto the server and do some file management
-sudo git clone https://github.com/smaxwellstewart/ec2-nginx-setup
 sudo mv ec2-nginx-setup/* .
 sudo rm -rf ec2-nginx-setup/ README.md
 
 # Modify the setup files to use your own site name. Replace put-your-site-name-here with your site name (Do not include top level domain).
 sed -ie "s/sitename/$sitenamev/g" setup.sh
-sed -ie "s/sitename/$sitenamev/g" laravel.conf
+sed -ie "s/sitename/$sitenamev/g" codeigniter.conf
 
 # Modify the setup files to use your server name. Replace put-your-server-name-here with your server name.
 sed -ie "s/hostnameholder/$(hostname)/g" setup.sh
@@ -37,7 +27,7 @@ sed -i "s|githolder|${repo}|" setup.sh
 
 # Do some file clean up.
 sudo rm setup.she
-sudo rm laravel.confe
+sudo rm codeigniter.confe
 
 # Update the setup.sh permissons and then run the shell script
 sudo chmod 777 setup.sh
